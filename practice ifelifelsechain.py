485c31342457695a47159da09243c8fcd184b755
Troy Shibukawa <24emmory@gmail.com>
2020-07-07 22:09:07 -0700

    Add new practice file

---
+++ practice ifelifelsechain.py
@@ -0,0 +1,14 @@
+age = 30
+
+if age < 12:
+    print("You are not admitted to the show.")
+
+elif age < 18:
+    print("In one year you will be admitted to the show.")
+
+else:
+    print("Congratulations! You can now be addmited to the show!")
+
+
+
+
