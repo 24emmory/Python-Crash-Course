2c7fa063d3c8653f38a41f966d195422e66f6758
Troy Shibukawa <24emmory@gmail.com>
2020-07-08 01:52:23 -0700

    upload practice ifelifelse chain

---
+++ setpriceifelifelsechain.py
@@ -0,0 +1,23 @@
+age = 12
+
+if age < 4:
+    price = 0
+
+elif age < 18:
+    price = 25
+
+else:
+    price = 40
+
+print(f"Your admission cost is ${price}.")
+
+#instead of
+age = 12
+if age < 4:
+    Print("Your admission cost is $0")
+
+elif age < 18:
+    print("Your admission cost is $25")
+
+else:
+    print("Your admission cost is $40")
