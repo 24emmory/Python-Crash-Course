0c44d26b5e5f5d2ccedc860d7e824dd40e9f43c2
Troy Shibukawa <24emmory@gmail.com>
2020-07-08 23:35:28 -0700

    push example of if multiple conditions

---
+++ multipleconditionstest.py
@@ -0,0 +1,15 @@
+requested_toppings = ['mushrooms','extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+#If statements are used in succession to find multiple conditions, whereas
+#elif and else statements are used to specify and find one condition.
