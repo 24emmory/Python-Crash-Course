9633c4ffcc4a8da3db6e2005acbd3b5aeb5d9c38
Troy Shibukawa <24emmory@gmail.com>
2020-07-10 23:02:26 -0700

    pushing first two parts of problem 5-4

---
+++ 54aliencolors2.py
@@ -0,0 +1,11 @@
+alien_color = ['green']
+
+if 'green' in alien_color:
+    print("Player has just earned 5 points for shooting an alien!")
+
+blunder = 'red'
+
+if blunder != 'green':
+    print("Player has just earned 5 points for shooting an alien!")
+
+
