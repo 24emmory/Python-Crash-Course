691c98f076feddfd72bffc23b94de9b858fef715
Troy Shibukawa <24emmory@gmail.com>
2020-07-10 23:12:33 -0700

    pushing second part of 5-4 problem

---
+++ 54aliencolors2pt2.py
@@ -0,0 +1,15 @@
+alien_color = 'Red'
+
+if 'Red' in alien_color:
+    print("Player just earned 5 points for shooting alien")
+
+if alien_color != 'Red':
+    print("\nPlayer just earned 5 points for shooting alien")
+
+
+alien_colors = 'Red'
+
+if 'Red' in alien_colors:
+    print("\nPlayer just earned 5 points for shooting alien")
+else:
+    print("\nPlayer just earned 5 points for shooting alien")
