5e096d9bdf1112f0c1fedc387a6c0dc9567d287a
Troy Shibukawa <24emmory@gmail.com>
2020-07-11 16:35:20 -0700

    Pushing exercise favoritefruits 5 7

---
+++ favoritefruit57.py
@@ -0,0 +1,19 @@
+#make a list of your favorite fruits, write a series of independent if
+#statements that check for certain fruits in your list
+
+favorite_fruits = ['oranges','apples','peaches']
+
+if 'oranges' in favorite_fruits:
+    print(f"You really like {favorite_fruits[0]}!")
+
+if 'cantaloupes' in favorite_fruits:
+    print("You really like cantaloupes!")
+
+if 'apples' in favorite_fruits:
+    print(f"You really like {favorite_fruits[1]}!")
+
+if 'mangos' in favorite_fruits:
+    print("You really like mangos!")
+
+if 'peaches' in favorite_fruits:
+    print(f"You really like {favorite_fruits[2]}!")
