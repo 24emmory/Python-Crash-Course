4985da1c906c6d5933234fa0ea0fec978c7425c5
Troy Shibukawa <24emmory@gmail.com>
2020-07-13 21:40:25 -0700

    Example of checking list vacancy before program runs with an if statement!

---
+++ checklistemptyexercise.py
@@ -0,0 +1,8 @@
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requeste_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making yor pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
