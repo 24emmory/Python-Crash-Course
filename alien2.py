60d74ce1ff6d0286c71c0252c25bb55054d623e5
Troy Shibukawa <24emmory@gmail.com>
2020-07-16 13:50:28 -0700

    pushing empty dictionary example/how to add key value-pairs

---
+++ alien2.py
@@ -0,0 +1,9 @@
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+#no parenthesis around each value as would be done in functions. Number is bare,
+#string has single quotations
+
+print(alien_0)
