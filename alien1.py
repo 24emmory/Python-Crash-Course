a3a2d814cc02d6195ff9901bba34720ad0aaf8cb
Troy Shibukawa <24emmory@gmail.com>
2020-07-16 02:20:48 -0700

    Pushing example usage and practice of defining dictionaries/adding key-value pairs

---
+++ alien1.py
@@ -0,0 +1,21 @@
+alien_0 = {'color': 'green', 'points': 5}
+#the way you encapsulate the elements determines if its a (tuple){dict}[list]
+print(alien_0)
+
+#adding key-value pairs to dictionary, much like list[1] = B
+#calling the dictionary, ['quoting the key'] & assigning to the pair
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+#defining same dictionary
+#print dictionary
+#added new key-value pair to dictionary: key 'x_position' & value 0, same for y.
+
+dictionary = {'length':'3m','height':'4m'}
+print(dictionary)
+print(dictionary['length'])
+
+#quote each variable/element, use a colon to seperate key from value, and a
+#comma to seperate value-key pairs
