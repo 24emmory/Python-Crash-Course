e688da8e7026baa07a61ddecaedad95c299faa9a
Troy Shibukawa <24emmory@gmail.com>
2020-07-16 13:58:27 -0700

    Pushing example of changing value in key-value pair as well as exercise of adding a key-value pair to a dictionary. Very similar to adding and changing indexes

---
+++ alien3.py
@@ -0,0 +1,14 @@
+alien_0 = {'color': 'green'}
+print(f"The alien color is {alien_0['color']}")
+#apostraphies are huge in dictionaries, around everything. & colons for key/valu
+#e pairs
+
+alien_0['color'] = 'yellow'
+print(f"The alien color is now {alien_0['color']}")
+#typing in the key to access the value!
+
+#adding a key-value pair
+alien_0['points'] = 5
+print(f"The alien point value is now {alien_0['points']}")
+
+print(alien_0)
