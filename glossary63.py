5a8c9656a5b97c6d2a8c1167d6c44df4b14f72fa
Troy Shibukawa <24emmory@gmail.com>
2020-07-17 15:34:37 -0700

    pushing example using proper formatting to display a python dictionarys usability as an actual virtual dictionary

---
+++ glossary63.py
@@ -0,0 +1,10 @@
+glossary = {'tuples':'unchangeable lists','key_value_pair':'used in dictionaries'
+            ,'square_bracket_notation':'used to get a value from a key-value',
+            'get_method':'used to get a value from a key-value','elif_chain':
+            'used to execute precise conditions'}
+
+print(f"The term is tuples:{glossary['tuples']}\n")
+print(f"The term is key-value pair:{glossary['key_value_pair']}\n")
+print(f"The term is square bracket notation{glossary['square_bracket_notation']}\n")
+print(f"The term is get method:{glossary['get_method']}\n")
+print(f"The term is elif chain:{glossary['elif_chain']}")
