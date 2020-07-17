0d1d48fac0ef5be9cfd2f9136e4dd258027608d4
Troy Shibukawa <24emmory@gmail.com>
2020-07-17 00:46:47 -0700

    pushing exercise where I stored several peoples favorite numbers and printed them to the screen using string format and square bracket method to access the value from the dictionarys key-value pair (KVP)

---
+++ favoritenumbers62.py
@@ -0,0 +1,6 @@
+favorite_numbers = {'tom': 2,'jerry': 8, 'bobby':4,'donald': 3, 'barry': 9}
+print(f"Toms favorite number is {favorite_numbers['tom']}")
+print(f"Jerrys favorite number is {favorite_numbers['jerry']}")
+print(f"Bobbys favorite number is {favorite_numbers['bobby']}")
+print(f"Donalds favorite number is {favorite_numbers['donald']}")
+print(f"Barrys favorite number is {favorite_numbers['barry']}")
