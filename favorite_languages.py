9e31c94cfe16d0fffdf5fc939a6f1ea951b0e7d0
Troy Shibukawa <24emmory@gmail.com>
2020-07-16 17:57:51 -0700

    pushing example of using dictionaries to hold one piece of information about different objects instead of different info on 1 object ex) alien and its color and points compared to food preference of 20 differnt people

---
+++ favorite_languages.py
@@ -0,0 +1,10 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'C',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+sarah_favorite = favorite_languages['sarah']
+
+print(f"Sarah's favoritelange is {sarah_favorite}!")
