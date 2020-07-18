8a53ef8ef531183b42cf5b89fceca702ef75bfa0
Troy Shibukawa <24emmory@gmail.com>
2020-07-17 21:07:01 -0700

    Pushing example of using items method in a for loop to access a key-value pair

---
+++ user.py
@@ -0,0 +1,9 @@
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+#item method positional matches the two variables with each of the two key-value
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
