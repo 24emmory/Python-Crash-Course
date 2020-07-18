f742cd91f610b3520c13d00309c077e438add043
Troy Shibukawa <24emmory@gmail.com>
2020-07-17 21:41:57 -0700

    pushing example of default behavior while looping through library vs using the key() method. both behaviors are the same

---
+++ favorite_languages3.py
@@ -0,0 +1,17 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c' ,
+    'edward': 'ruby' ,
+    'phil': 'python' ,
+}
+
+for name in favorite_languages:
+    print(name)
+
+print("\n")
+#same as
+
+for name in favorite_languages.keys():
+    print(name)
+
+#when looping through a dictionary!
