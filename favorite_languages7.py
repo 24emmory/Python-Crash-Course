1c63d047aa970a9daa527ae73ac534ef436dcef7
Troy Shibukawa <24emmory@gmail.com>
2020-07-18 23:59:08 -0700

    Pushing example of how to sort keys as they're returned in the for loop

---
+++ favorite_languages7.py
@@ -0,0 +1,11 @@
+favorite_languages = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python',
+}
+
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you fr taking the poll.")
+
+    #sorting the keys as they're returned* in the for loop
