66a7f8085fb7d819dbb4c37753ac2c91624fba79
Troy Shibukawa <24emmory@gmail.com>
2020-07-18 23:52:30 -0700

    Pushing file showing utility of using keys() method to check what objects and/or keys are not in a listing by using 'not in'git status!

---
+++ favorite_languages6.py
@@ -0,0 +1,13 @@
+favorite_languages = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python',
+}
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+#example of using keys() method to run through key-value pairs to find out what
+#key was 'not in' the list! We can use it for finding objects in the list, and
+#objects not in the list!
