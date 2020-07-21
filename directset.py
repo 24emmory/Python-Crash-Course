e3394899119e28889e1a602c602b1266b0317fc3
Troy Shibukawa <24emmory@gmail.com>
2020-07-21 16:07:51 -0700

    Pushing example of how to tell the differnce between dictionaries and sets which both have curly braces

---
+++ directset.py
@@ -0,0 +1,7 @@
+languages = {'python','ruby','python','c'}
+print(languages)
+
+#building a set directly to handle multiples. sets look like dictionaries, but
+#are not. you can differentiate between the two by looking to see if there is
+#a key-value pair, or if its just items alone. Similar to dictionaries because
+#of curly brackets
