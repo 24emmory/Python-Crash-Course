ac25c63c46234c3ac1ad2214f9ca2b11bc66f75f
Troy Shibukawa <24emmory@gmail.com>
2020-07-20 23:40:33 -0700

    Commiting file practicing looping through just values in a dictionary using the .value() method!

---
+++ loopall1.py
@@ -0,0 +1,10 @@
+favorite_foods = {
+    'Tom':'Hot Dogs',
+    'Jessica':'Muffins',
+    'Ryan':'Scrambled Eggs',
+    'Kevin': 'Pizza',
+}
+
+print("Everyone really enjoyed these foods at the party:")
+for food in favorite_foods.values():
+    print(language.title())
