656af7d3dcc18df081523b8667615829f13a5967
Troy Shibukawa <24emmory@gmail.com>
2020-07-20 23:46:21 -0700

    Pushing exercise example where im looping through all the values in a dictionary using a for loop, where the for pulls each value from the dictionary and assigns it to the variable stated. The for* does a lot of work here!

---
+++ loopvalues.py
@@ -0,0 +1,12 @@
+favorite_places = {
+    'Sarah':'England',
+    'Jessica':'Sofa',
+    'Tim':'Class',
+    'Daniel': 'Church',
+}
+
+print("The survey said that people liked visiting these places most:")
+for places in favorite_places.values():
+    print(places)
+
+#the for statement here pulls each value from the dictionary and assigns it to the variable places
