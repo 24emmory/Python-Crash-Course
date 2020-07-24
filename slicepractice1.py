4394a907165a4f6b225ffaa01dda8fbdecd0c04b
Troy Shibukawa <24emmory@gmail.com>
2020-07-23 23:49:33 -0700

    Practcing how to slice as a reminder

---
+++ slicepractice1.py
@@ -0,0 +1,11 @@
+passage = ['ave','blvd','road', 'street']
+my_passage = passage[:]
+
+print("Names of pathways on the road:")
+print(my_passage)
+
+
+#the exercise
+print(my_passage[2:3])
+print(my_passage[:])
+print(my_passage[1:4])
