5650b24666c7254af4edd8958ffd80bb4633ce5f
Troy Shibukawa <24emmory@gmail.com>
2020-08-01 15:46:17 -0700

    Pushing practice loop stuff, revisiting old code

---
+++ practiceforloop.py
@@ -0,0 +1,11 @@
+#alien revisit
+#type of collection determined by what surrounds them, lists [], tuples (), dicts {}
+alien1 = {'color':'green','points':5}
+alien2 = {'color':'yellow','points':10}
+alien3 = {'color':'red','points':15}
+
+#do not quote if meant literal
+aliens = [alien1,alien2,alien3]
+
+for alien in aliens:
+    print(alien)
